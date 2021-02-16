b6340cc766a412e92c46fdacb25e0da41e27ad7f
Franco Canzani <francocanzani@gmail.com>
2021-02-16 00:32:19 -0300

    VSC

--- gessNumber.py
+++ gessNumber.py
@@ -13,4 +13,23 @@ def guess(x):
 
     print('Congrats! You just guesses the number')
 
-guess(10)
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low 
+        feedback = input(f'Is {guess} too high (H), too low (L) or correct (c)?').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+            
+    print(f'The computer guessed your number.')
+
+computer_guess(10)
+
+#guess(10)
